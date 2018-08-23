a0901d2e6a723010197c10ad5f96e8c94404d205
kanekotic <alvarojosepl@gmail.com>
2018-08-23 22:28:33 +0200

    updated dependencies and added gradle build

---
+++ android/src/test/java/com/reactlibrary/LeaderboardModuleTest.java
@@ -0,0 +1,14 @@
+package com.reactlibrary;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class LeaderboardModuleTest {
+
+    @Test
+    public void testSomething() {
+        assertThat(true, is(true));
+    }
+}
