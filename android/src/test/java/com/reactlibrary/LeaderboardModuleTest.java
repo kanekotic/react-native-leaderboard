a93342e2628bcda740814be7bd16985538f8a41b
kanekotic <alvarojosepl@gmail.com>
2018-08-23 22:32:39 +0200

    added example of mockito

--- android/src/test/java/com/reactlibrary/LeaderboardModuleTest.java
+++ android/src/test/java/com/reactlibrary/LeaderboardModuleTest.java
@@ -1,14 +1,33 @@
 package com.reactlibrary;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LeaderboardModuleTest {
+    private static final String FAKE_STRING = "HELLO WORLD";
+
+    @Mock
+    Context mMockContext;
 
     @Test
     public void testSomething() {
+        when(mMockContext.getString(R.string.catalyst_debugjs))
+                .thenReturn(FAKE_STRING);
+
         assertThat(true, is(true));
     }
 }
