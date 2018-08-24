2496fd810c6d7080125957b60c27b04d38446000
kanekotic <alvarojosepl@gmail.com>
2018-08-24 10:01:12 +0200

    added first example of implementation of leaderboard retrieving

--- android/src/main/java/com/reactlibrary/LeaderboardModule.java
+++ android/src/main/java/com/reactlibrary/LeaderboardModule.java
@@ -1,14 +1,24 @@
 
 package com.reactlibrary;
 
+import android.content.Intent;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class LeaderboardModule extends ReactContextBaseJavaModule {
 
+  private static final int RC_LEADERBOARD_UI = 9004;
+
+  private final ReactApplicationContext reactContext;
+
   public LeaderboardModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    this.reactContext = reactContext;
   }
 
   @Override
@@ -17,7 +27,15 @@ public class LeaderboardModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void show() {
+  public void show(String LeaderboardId) {
+    Games.getLeaderboardsClient(this.reactContext, GoogleSignIn.getLastSignedInAccount(this.reactContext))
+            .getLeaderboardIntent(LeaderboardId)
+            .addOnSuccessListener(new OnSuccessListener<Intent>() {
+              @Override
+              public void onSuccess(Intent intent) {
+//                startActivityForResult(intent, RC_LEADERBOARD_UI);
+              }
+            });
 
   }
 }
